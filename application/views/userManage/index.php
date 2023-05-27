49d234734c6cc3461d916bb0871ef553b3ae48e6
Nazario Safariesqi <nazariotyo@gmail.com>
2023-05-27 19:55:28 +0700

    Fix CRUD User Management

--- application/views/userManage/index.php
+++ application/views/userManage/index.php
@@ -6,7 +6,16 @@
 
     <div class="row">
         <div class="col-lg-6">
-            <a href="#" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">Add New User</a>
+            <?= form_error('name', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= form_error('email', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= form_error('password', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= form_error('name-edit', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= form_error('email-edit', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= form_error('password1', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= form_error('password2', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= $this->session->flashdata('message'); ?>
+
+            <a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#newUserModal">Add New User</a>
             <table class="table table-hover">
                 <thead>
                     <tr>
@@ -20,28 +29,16 @@
                 <tbody>
                     <?php $i = 1 ?>
                     <?php foreach ($user as $u) : ?>
-
                         <tr>
-
                             <th scope="row"><?= $i++; ?></th>
-
-                            <td><?= $u->name; ?></td>
-                            <td><?= $u->email; ?></td>
-                            <td><?php if ($u->role_id == 1) {
-                                    echo "Administrator";
-                                } else if ($u->role_id == 2) {
-                                    echo "Member";
-                                } else {
-                                    echo "-";
-                                } ?>
-                            </td>
+                            <td><?= $u['name']; ?></td>
+                            <td><?= $u['email']; ?></td>
+                            <td><?= $u['role']; ?></td>
                             <td>
-                                <a href="" class="badge badge-success">Edit</a>
-                                <a href="" class="badge badge-danger">Delete</a>
+                                <a href="" class="badge badge-success" id="btn-edit" data-id="<?= $u['id']; ?>" data-name="<?= $u['name']; ?>" data-email="<?= $u['email']; ?>" data-toggle="modal" data-target="#editUserModal">Edit</a>
+                                <a href="" class="badge badge-danger" data-toggle="modal" data-target="#deleteUserModal"> Delete</a>
                             </td>
-
                         </tr>
-
                     <?php endforeach; ?>
                 </tbody>
             </table>
@@ -54,38 +51,102 @@
 </div>
 <!-- End of Main Content -->
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal Add New User -->
+<div class="modal fade" id="newUserModal" tabindex="-1" role="dialog" aria-labelledby="newUserModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add New User</h5>
+                <h5 class="modal-title" id="newUserModalLabel">Add New User</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="">
+            <form action="<?= base_url('UserManage/insert'); ?>" method="POST">
                 <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="exampleInputName">Name</label>
-                            <input type="name" class="form-control" id="name" placeholder="Enter name">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="Password">
-                        </div>
-                    </form>
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="role_id">Role</label>
+                        <select class="form-control w-26" id="role_id" name="role_id">
+                            <option value="1">Administrator</option>
+                            <option value="2">Member</option>
+                        </select>
+                    </div>
+                    <div class="password">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Add</button>
                 </div>
             </form>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Edit / Update -->
+<div class="modal fade" id="editUserModal" tabindex="-1" role="dialog" aria-labelledby="editUserModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editUserModalLabel">Edit User Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('UserManage/update'); ?>" method="POST">
+                <div class="modal-body">
+                    <input type="hidden" name="user_id" id="user_id">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name-edit" name="name-edit" placeholder="Edit name" id="user_id" data-id="<?= $u['name'] ?>;">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email address</label>
+                        <input type="email" class="form-control" id="email-edit" name="email-edit" aria-describedby="emailHelp" placeholder="Edit email" id="user_id" data-id="<?= $u['email'] ?>;">
+                    </div>
+                    <div class="password">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password1" name="password1" placeholder="New password">
+                    </div>
+                    <div class="password">
+                        <input type="password" class="form-control" id="password2" name="password2" placeholder="Re-type password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" name="update" class="btn btn-primary">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Delete User-->
+<div class="modal fade" id="deleteUserModal" tabindex="-1" role="dialog" aria-labelledby="deleteUserModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteUserModalLabel">Delete User</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to delete this user?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-danger" href="<?= base_url('UserManage/delete/' . $u['id']); ?>">Delete</a>
+            </div>
         </div>
     </div>
 </div>
