a5e0504d500446786ca9dc76a73c8300c884df5c
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-03 22:55:38 +0700

    Add search feature for editRuangan

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -215,7 +215,85 @@ class Admin extends CI_Controller
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar2', $data);
+        $this->load->view('templates/topbarRuangan', $data);
+        $this->load->view('admin/editRuangan', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function SearchRuangan()
+    {
+        $data['title'] = 'Edit Ruangan';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $keyword = $this->input->get('keyword');
+
+        if ($keyword) {
+            $this->session->set_userdata('search_keyword', $keyword);
+        } else {
+            $keyword = $this->session->userdata('search_keyword');
+        }
+
+        $config['base_url'] = base_url('Admin/Ruangan'); // URL base halaman
+        $config['total_rows'] = $this->db->like('no_ruang', $keyword)
+            ->or_like('nama_ruang', $keyword)
+            ->or_like('gateway', $keyword)
+            ->or_like('lantai', $keyword)
+            ->count_all_results('ruangan'); // Jumlah total data yang akan dipaginasi
+        $config['per_page'] = 7; // Jumlah data per halaman
+        $config['uri_segment'] = 3; // URI segment yang menyimpan nomor halaman
+        $config['num_links'] = 1; // Jumlah link pagination yang ditampilkan di sekitar halaman aktif
+        $config['use_page_numbers'] = TRUE; // Menggunakan nomor halaman bukan offset
+
+        //style
+        $config['full_tag_open'] = '<nav><ul class="pagination">';
+        $config['full_tag_close'] = '</ul></nav>';
+
+        $config['first_link'] = 'First';
+        $config['first_tag_open'] = '<li class="page-item">';
+        $config['first_tag_close'] = '</li>';
+
+        $config['last_link'] = 'Last';
+        $config['last_tag_open'] = '<li class="page-item">';
+        $config['last_tag_close'] = '</li>';
+
+        $config['next_link'] = '&raquo;';
+        $config['next_tag_open'] = '<li class="page-item">';
+        $config['next_tag_close'] = '</li>';
+
+        $config['prev_link'] = '&laquo;';
+        $config['prev_tag_open'] = '<li class="page-item">';
+        $config['prev_tag_close'] = '</li>';
+
+        $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+
+        $config['num_tag_open'] = '<li class="page-item">';
+        $config['num_tag_close'] = '</li>';
+
+        $config['attributes'] = array('class' => 'page-link');
+
+        $this->pagination->initialize($config);
+
+        $limit = $config['per_page'];
+        $page = $this->uri->segment(3) ? $this->uri->segment(3) : 1;
+        $offset = ($page - 1) * $limit;
+
+        $this->db->limit($limit, $offset);
+        $this->db->group_start()
+            ->like('no_ruang', $keyword)
+            ->or_like('nama_ruang', $keyword)
+            ->or_like('gateway', $keyword)
+            ->or_like('lantai', $keyword)
+            ->group_end();
+        $this->db->order_by('lantai', 'asc');
+
+        $data['ruangan'] = $this->db->get('ruangan')->result_array();
+        $data['pagination'] = $this->pagination->create_links();
+        $data['keyword'] = $keyword;
+        $data['offset'] = $offset;
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbarRuangan', $data);
         $this->load->view('admin/editRuangan', $data);
         $this->load->view('templates/footer');
     }
@@ -241,7 +319,7 @@ class Admin extends CI_Controller
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar2', $data);
+            $this->load->view('templates/topbarRuangan', $data);
             $this->load->view('Admin/editRuangan', $data);
             $this->load->view('templates/footer');
         } else {
@@ -287,7 +365,7 @@ class Admin extends CI_Controller
         if ($this->form_validation->run() == FALSE) {
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar2', $data);
+            $this->load->view('templates/topbarRuangan', $data);
             $this->load->view('Admin/editRuangan', $data);
             $this->load->view('templates/footer');
         } else {
