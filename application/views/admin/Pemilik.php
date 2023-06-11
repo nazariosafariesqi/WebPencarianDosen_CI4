1575b74921747cf6a462d05e2fb249b4f541337a
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-11 12:22:44 +0700

    Add search to dropdown options in menu Pemilik

--- application/views/admin/Pemilik.php
+++ application/views/admin/Pemilik.php
@@ -66,7 +66,7 @@
                         <label for="mac_address">Mac Address</label>
                         <select class="form-control" id="mac_address" name="mac_address">
                             <?php foreach ($leases as $lease) : ?>
-                                <option value="<?= $lease['mac_address'] ?>"><?= $lease['mac_address'] ?></option>
+                                <option value="<?= $lease['mac_address'] ?>" data-search="<?= strtolower($lease['mac_address']) ?>"><?= $lease['mac_address'] ?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
@@ -93,6 +93,34 @@
     </div>
 </div>
 
+<script>
+    function searchMacAddress() {
+        // Mengambil input value
+        var searchInput = document.getElementById('search_mac').value.toLowerCase();
+
+        // Elemen untuk memilih dan opsinya
+        var selectElement = document.getElementById('mac_address');
+        var options = selectElement.options;
+
+        // Perulangan dari opsinya dan show/hide berdasarkan inputan
+        for (var i = 0; i < options.length; i++) {
+            var option = options[i];
+            var macAddress = option.dataset.search.toLowerCase();
+
+            // Menentukan apakah setiap 12 karakter dari opsi cocok dengan pencarian
+            if (macAddress.substr(0, searchInput.length) === searchInput || searchInput === '') {
+                option.style.display = '';
+            } else {
+                option.style.display = 'none';
+            }
+        }
+    }
+
+    // Panggil fungsi searchMacAddress ke event keyup input pencarian
+    document.getElementById('search_mac').addEventListener('keyup', searchMacAddress);
+</script>
+
+
 <!-- Modal Edit / Update -->
 <div class="modal fade" id="editPemilikModal" tabindex="-1" role="dialog" aria-labelledby="editPemilikModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
@@ -107,12 +135,16 @@
                 <div class="modal-body">
                     <input type="hidden" name="user_id" id="user_id">
                     <div class="form-group">
-                        <label for="nama-pemilik">Nama Pemilik</label>
-                        <input type="text" class="form-control" id="nama-pemilik" name="nama-pemilik" placeholder="Edit Nama Pemilik" id="user_id" data-id="<?= $p['nama_pemilik'] ?>;">
+                        <label for="mac-address">Mac Address</label>
+                        <select class="form-control" id="mac-address" name="mac-address">
+                            <?php foreach ($leases as $lease) : ?>
+                                <option value="<?= $lease['mac_address'] ?>" data-search="<?= strtolower($lease['mac_address']) ?>"><?= $lease['mac_address'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
                     </div>
                     <div class="form-group">
-                        <label for="mac-address">MAC Address</label>
-                        <input type="text" class="form-control" id="mac-address" name="mac-address" placeholder="Edit MAC Address" id="user_id" data-id="<?= $p['mac_address'] ?>;">
+                        <label for="nama-pemilik">Nama Pemilik</label>
+                        <input type="text" class="form-control" id="nama-pemilik" name="nama-pemilik" placeholder="Edit Nama Pemilik" id="user_id" data-id="<?= $p['nama_pemilik'] ?>;">
                     </div>
                     <div class="form-group">
                         <label for="jenis_id">Jenis</label>
@@ -132,6 +164,33 @@
         </div>
     </div>
 </div>
+
+<script>
+    function searchMacAddress() {
+        // Mengambil input value
+        var searchInput = document.getElementById('search_mac').value.toLowerCase();
+
+        // Elemen untuk memilih dan opsinya
+        var selectElement = document.getElementById('mac-address');
+        var options = selectElement.options;
+
+        // Perulangan dari opsinya dan show/hide berdasarkan inputan
+        for (var i = 0; i < options.length; i++) {
+            var option = options[i];
+            var macAddress = option.dataset.search.toLowerCase();
+
+            // Menentukan apakah setiap 12 karakter dari opsi cocok dengan pencarian
+            if (macAddress.substr(0, searchInput.length) === searchInput || searchInput === '') {
+                option.style.display = '';
+            } else {
+                option.style.display = 'none';
+            }
+        }
+    }
+
+    // Panggil fungsi searchMacAddress ke event keyup input pencarian
+    document.getElementById('search_mac').addEventListener('keyup', searchMacAddress);
+</script>
 
 <!-- Modal Delete Pemilik-->
 <div class="modal fade" id="deletePemilikModal" tabindex="-1" role="dialog" aria-labelledby="deletePemilikModalLabel" aria-hidden="true">
