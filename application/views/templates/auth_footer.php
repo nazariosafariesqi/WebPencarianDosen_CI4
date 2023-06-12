900f66e3cb9fc29e2fe2d065c2c95775babfa73c
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-12 18:31:06 +0700

    Fix error in auth footer

--- application/views/templates/auth_footer.php
+++ application/views/templates/auth_footer.php
@@ -6,7 +6,7 @@
 <script src="<?= base_url('vendor/') ?>jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url('vendor/') ?>js/sb-admin-2.min.js"></script>
+<script src="<?= base_url('assets/') ?>js/sb-admin-2.min.js"></script>
 
 </body>
 
