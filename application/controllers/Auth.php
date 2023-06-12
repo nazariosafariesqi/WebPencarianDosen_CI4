03d6d33d252a2f04b11c4ef52ed098a5e4e1a7bd
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-12 19:12:03 +0700

    test

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -30,6 +30,14 @@ class Auth extends CI_Controller
         }
     }
 
+    public function index()
+    {
+        $data['title'] = 'Login Page';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
     public function login()
     {
         $email = $this->input->post('email');
