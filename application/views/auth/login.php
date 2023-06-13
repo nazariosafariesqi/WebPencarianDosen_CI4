7085c2709eabdca8dde432316ae9a58013d9221c
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-13 12:01:23 +0700

    Check error in Auth

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,7 +18,7 @@
                                     </div>
 
                                     <?= $this->session->flashdata('message'); ?>
-                                    <form class="user" method="post" action="<?= base_url('auth'); ?>">
+                                    <form class="user" method="post" action="<?= base_url('http://192.168.60.80/auth'); ?>">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
                                         </div>
