5e2ff63ad1ec6145bfce4a5a5ba42ea8f6eaaa39
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-13 15:15:19 +0700

    Set icon for server

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,6 +10,7 @@
     <meta name="author" content="">
 
     <title><?= $title; ?></title>
+    <link rel="icon" type="image/ico" href="<?= base_url('Loc.ico') ?>">
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
