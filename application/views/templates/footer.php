5ad1d3e988f29abcada8461c1be40030830658a2
Nazario Safariesqi <nazariotyo@gmail.com>
2023-06-13 15:59:35 +0700

    Setting icon header & footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; JTI Polinema <?= date('Y'); ?></span>
+            <span>Copyright &copy; Nazario Safariesqi <?= date('Y'); ?></span>
         </div>
     </div>
 </footer>
