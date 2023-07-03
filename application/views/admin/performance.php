ff9a5dd3a41c45b62408ea18618ef30434d11861
Nazario Safariesqi <nazariotyo@gmail.com>
2023-07-03 17:25:20 +0700

    Add Menu Search Performance

---
+++ application/views/admin/performance.php
@@ -0,0 +1,40 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg">
+
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">Lama Eksekusi</th>
+                        <th scope="col">Jumlah Data</th>
+                        <th scope="col">Tanggal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = $offset + 1; ?>
+                    <?php foreach ($eksekusi as $e) : ?>
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td><?= $e['lama_eksekusi']; ?></td>
+                            <td><?= $e['jumlah_data']; ?></td>
+                            <td><?= $e['waktu']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+            <?= $this->pagination->create_links(); ?>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
