fba007e6946e4d231d86482faefc82e46dda56dc
Nazario Safariesqi <nazariotyo@gmail.com>
2023-07-04 13:18:22 +0700

    Testing Search feature data limit

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -16,15 +16,15 @@ class User extends CI_Controller
         if (empty($keyword)) {
             $results = []; // Jika keyword kosong
         } else {
-            $sql = "(SELECT p.nama_pemilik, p.jenis, l.mac_address, l.ip_address, l.last_seen, r.nama_ruang, r.lantai, DATE_FORMAT(l.waktu_ambil, '%d-%m-%Y %H:%i:%s') AS waktu_ambil
+            $sql = "(SELECT p.nama_pemilik, p.jenis, l.mac_address, l.ip_address, l.last_seen, r.nama_ruang, r.lantai, DATE_FORMAT(l.waktu_ambil, '%d-%m-%Y') AS waktu_ambil
             FROM pemilik p
             JOIN leases l ON p.mac_address = l.mac_address
             JOIN ip i ON l.ip_address = i.ip_address
             JOIN ruangan r ON i.ruangan_id = r.id
             WHERE p.nama_pemilik LIKE '%" . $keyword . "%'
-                
+                AND DATE(l.waktu_ambil) = CURDATE()
             ORDER BY l.waktu_ambil ASC, l.last_seen ASC
-            LIMIT 500)";
+            LIMIT 5)";
 
             $results = $this->db->query($sql)->result_array();
         }
